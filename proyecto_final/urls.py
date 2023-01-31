6aaf250fa91e88d729020e4aceb816f2906caa7a
Eugenio Garcia <eugeniog288@gmail.com>
2023-01-31 20:05:48 -0300

    Se agregaron algunos templeates y se comienza con la app de registro

--- proyecto_final/urls.py
+++ proyecto_final/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from tecnoreview.views import home
+from registro.views import registro
 
 urlpatterns = [
+    path('', home, name="home"),
     path('admin/', admin.site.urls),
+    path('registro/', registro, name="registro"),
 ]
--- registro/views.py
+++ registro/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from datetime import datetime
 
-# Create your views here.
+from django.db.models import Q
+from django.shortcuts import render, redirect
+from django.urls import reverse, reverse_lazy
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth import login, authenticate
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from registro.forms import UserRegisterForm
+
+
+def registro(request):
+    if request.method == "POST":
+        formulario = UserRegisterForm(request.POST)
+
+        if formulario.is_valid():
+            formulario.save()  # Esto lo puedo usar porque es un model form
+            url_exitosa = reverse('home')
+            return redirect(url_exitosa)
+    else:  # GET
+        formulario = UserRegisterForm()
+    return render(
+        request=request,
+        template_name='registro/registro.html',
+        context={'form': formulario},
+    )
